24872e9a818c1418a5e79d2806630ad02202c2fe
BlueDuck-jpg <73210133+BlueDuck-jpg@users.noreply.github.com>
2021-11-22 21:04:42 +0800

    Update BeeTycoonLoader.lua

--- BeeTycoonLoader.lua
+++ BeeTycoonLoader.lua
@@ -1,10 +1,18 @@
 -- Checks if player claimed a tycoon
-if game.Players.LocalPlayer.Team ~= "For Hire" then
+if tostring(game.Players.LocalPlayer.Team) ~= "For Hire" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/RobloxScripts/main/BeeTycoonMain.lua"))()
 else
+    function createNotification(Title, Text, Duration)
+        game.StarterGui:SetCore("SendNotification", {
+            Title = Title,
+            Text = Text,
+            Duration = Duration
+        }) 
+    end
     local unclaimed = workspace.Tycoons.Tycoons:FindFirstChild("Touch to claim!", true)
     firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, unclaimed.Head, 0)
     wait()
     firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, unclaimed.Head, 1)
-    createNotification("NOTIFICATION!", "Claimed " + unclaimed.Parent.Parent.Name, 5)
+    createNotification("NOTIFICATION!", "Claimed " .. unclaimed.Parent.Parent.Name, 5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/RobloxScripts/main/BeeTycoonMain.lua"))()
 end
